99914b0e91e85c257aac45a507a9c33a7c352337
Victor Martinez Vidal Pereira <victor.pereira@luizalabs.com>
2021-10-10 21:06:31 -0300

    style: add return type to functions

--- runnertracker/app.py
+++ runnertracker/app.py
@@ -6,17 +6,17 @@ from runnertracker.application import RootView
 logger = get_logger(__name__)
 
 
-def run_app():
+def run_app() -> None:
     logger.info("Starting application...")
     app = init_app()
     web.run_app(app)
 
 
-async def init_app(argv: str = None):
+async def init_app() -> web.Application:
     app = web.Application()
     setup_routes(app)
     return app
 
 
-def setup_routes(app):
+def setup_routes(app: web.Application) -> None:
     app.router.add_view("/", RootView)
--- runnertracker/application/root/views.py
+++ runnertracker/application/root/views.py
@@ -4,5 +4,5 @@ from aiohttp import web
 
 
 class RootView(web.View):
-    async def get(self):
+    async def get(self) -> web.Response:
         return web.json_response({"healthy": True}, status=HTTPStatus.OK)
